730b7e3f1eb98327f3d9342de4fc15cf3e64f1ea
Tarun Upadhyay <95377904+tarun-upadhyay@users.noreply.github.com>
2024-03-30 19:54:19 +0530

    create navbar

---
+++ app/Components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height={30}
+      width={30}
+      src="/images/placeholder.jpg"
+      alt="Avatar"
+    ></Image>
+  );
+};
+
+export default Avatar;
